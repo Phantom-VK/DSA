8b34dfeeb8916217f1b84ecb0a6a622bc7111b4b
VikramadityaKhupse <2022bit052@sggs.ac.in>
2024-11-15 12:32:16 +0530

    max consecuticve ones

---
+++ leetcode/max_consecutive_ones.kt
@@ -0,0 +1,15 @@
+///https://leetcode.com/problems/max-consecutive-ones/
+class Solution {
+    fun findMaxConsecutiveOnes(nums: IntArray): Int {
+        var curr_max = 0 ; var max = 0
+        for(num in nums){
+            if(num == 1){
+                curr_max++
+                max = if(curr_max>max)curr_max else max
+            }else{
+                curr_max = 0
+            }
+        }
+        return max
+    }
+}
---
+++ leetcode/max_consucutive_ones.java
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/max-consecutive-ones/submissions/1453273006/
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int curr_max = 0 , max = 0;
+
+        for(int i : nums){
+            if(i == 1){
+                curr_max++;
+                if(curr_max > max){max =  curr_max;}
+            }else{
+                curr_max = 0;
+            }
+        }
+        return max;
+    }
+}
