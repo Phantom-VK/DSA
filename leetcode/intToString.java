0c8350a7efca35d496f1596a8fcdaffb57a38b67
VikramadityaKhupse <2022bit052@sggs.ac.in>
2024-11-17 09:41:13 +0530

    Hackerrank, string to int

---
+++ leetcode/intToString.java
@@ -0,0 +1,8 @@
+//
+//
+//There are multiple ways:
+//
+//        String.valueOf(number) (my preference)
+//        "" + number (I don't know how the compiler handles it, perhaps it is as efficient as the above)
+//        Integer.toString(number)
+//
