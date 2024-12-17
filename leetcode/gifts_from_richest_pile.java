d450767759c1351e8557d93f969716e2fd1959c3
Phantom-VK <vikramadityakhupse@gmail.com>
2024-12-17 22:43:12 +0530

    2558. Take Gifts From the Richest Pile (NOt solved)

---
+++ leetcode/gifts_from_richest_pile.java
@@ -0,0 +1,8 @@
+package leetcode;
+import java.util.PriorityQueue;
+import java.util.Comparator;
+//https://leetcode.com/problems/take-gifts-from-the-richest-pile/
+public class gifts_from_richest_pile {
+
+    //need to solve
+}
