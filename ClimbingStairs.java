70d98112eb0b115aa93222012dcc166dcf5727aa
Phantom-VK <vikramadityakhupse@gmail.com>
2024-12-19 23:47:38 +0530

    You are climbing a staircase. It takes n steps to reach the top.
    Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?

---
+++ ClimbingStairs.java
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/climbing-stairs/
+public class ClimbingStairs {
+    public int climbStairs(int n) {
+        int prev1 = 1;
+        int prev2 = 1;
+
+        for (int i = 2; i <= n; ++i) {
+            final int dp = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = dp;
+        }
+
+        return prev1;
+    }
+}
