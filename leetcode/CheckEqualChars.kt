e87adf974ec91d34e86431a961041e6787ca3568
Phantom-VK <vikramadityakhupse@gmail.com>
2025-01-03 23:59:40 +0530

    1941. Check if All Characters Have Equal Number of Occurrences

---
+++ leetcode/CheckEqualChars.kt
@@ -0,0 +1,15 @@
+package leetcode
+
+//https://leetcode.com/problems/check-if-all-characters-have-equal-number-of-occurrences/description/?envType=problem-list-v2&envId=hash-table
+class Solution {
+    fun areOccurrencesEqual(s: String): Boolean {
+        val charCountMap = mutableMapOf<Char, Int>()
+
+        for (c in s) {
+            charCountMap[c] = charCountMap.getOrDefault(c, 0) + 1
+        }
+
+        val firstCount = charCountMap.values.firstOrNull() ?: return true
+        return charCountMap.values.all { it == firstCount }
+    }
+}
