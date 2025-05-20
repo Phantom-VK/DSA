d188f20ce98d275ccd82e6c4c92525ef2d6e15e5
Phantom-VK <vikramadityakhupse@gmail.com>
2025-05-20 20:12:07 +0530

    Leetcode Daily prblm

---
+++ leetcode/Zero_Array_Transformation_I.py
@@ -0,0 +1,13 @@
+#https://leetcode.com/problems/zero-array-transformation-i/description/?envType=daily-question&envId=2025-05-20
+class Solution:
+    def isZeroArray(self, nums: List[int], queries: List[List[int]]) -> bool:
+        d = [0] * (len(nums) + 1)
+        for l, r in queries:
+            d[l] += 1
+            d[r + 1] -= 1
+        s = 0
+        for x, y in zip(nums, d):
+            s += y
+            if x > s:
+                return False
+        return True
